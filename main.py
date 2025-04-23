fa9fe91d7d79880a8f55d8e91f40ba76c7618ada
Ritesh Sanjay <ritesh.sanjay@timesinternet.in>
2025-04-23 09:36:06 +0000

    main.py edited online with Bitbucket

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-import openpyxl
-from openpyxl import Workbook
-import xlrd
 import sys
 
 # ===== CONFIGURATION =====
