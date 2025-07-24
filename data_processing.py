0f086d7bbd6d37b4afed341026f20a454312277e
Ritesh Sanjay <ritesh.sanjay@timesinternet.in>
2025-07-24 12:30:11 +0000

    data_processing.py edited online with Bitbucket

--- data_processing.py
+++ data_processing.py
@@ -161,7 +161,7 @@ imp_commitment_data = fetch_imp_commitment_data()
 # Explicitly set the expected column names (update these if your sheet uses different names)
 IMP_PKGID_COL = 'Til_Package_Id__c'  # or whatever the actual column name is
 IMP_GEONAME_COL = 'Geo__c'           # or whatever the actual column name is
-IMP_VAL_COL = 'Max_Impressions__c'   # or whatever the actual column name is
+IMP_VAL_COL = 'Geo_Level_Imp__c'   # or whatever the actual column name is
 
 imp_lookup = {}
 if imp_commitment_data:
