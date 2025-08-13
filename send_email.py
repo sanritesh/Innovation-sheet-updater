6e0944636377f7342fd2685fd6ddeb77a4559050
Ritesh Sanjay <ritesh.sanjay@timesinternet.in>
2025-08-13 07:09:09 +0000

    send_email.py edited online with Bitbucket

--- send_email.py
+++ send_email.py
@@ -22,7 +22,7 @@ def send_notification():
 
     # Google Sheet URL (from env or default to new sheet)
     google_sheet_url = os.getenv(
-        'GOOGLE_APPS_SCRIPT_URL',
+        'GOOGLE_SHEET_URL',
         'https://docs.google.com/spreadsheets/d/1dp5WINj0Urrvk8Ul2rR_q6HDzjdeAp7iuw5IsY3J3f8/edit?gid=754847217'
     )
 
